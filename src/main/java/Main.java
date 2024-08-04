51c99ac948d56ee1aec3c8309398ec0c235c7cb5
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2024-08-04 13:54:30 +0200

    Update debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -56,13 +56,16 @@ public class Main {
                         out.print("*1\r\n$4\r\nping\r\n");
                         out.flush();
                         masterSocket.getInputStream().read();
+
                         out.print(
                             "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n");
                         out.flush();
                         masterSocket.getInputStream().read();
+
                         out.print("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
                         out.flush();
-                        // masterSocket.getInputStream().read();
+                        masterSocket.getInputStream().read();
+                        
                         out.print("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"); 
                         out.flush(); 
                         masterSocket.getInputStream().read();
