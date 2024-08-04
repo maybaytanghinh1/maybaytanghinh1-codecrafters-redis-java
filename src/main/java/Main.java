d20b6aa16fccc3271544814c93ff2d1ee38e7c8f
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2024-08-04 13:50:40 +0200

    comment out some code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,8 +63,9 @@ public class Main {
                         out.print("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
                         out.flush();
                         masterSocket.getInputStream().read();
-                        out.print("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"); 
-                        out.flush(); 
+                        // out.print("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"); 
+                        // out.flush(); 
+                        // masterSocket.getInputStream().read();
                     } catch (IOException e) {
                         System.err.println("Failed to connect to the master at " + master_host + ":" + master_port);
                         e.printStackTrace();
