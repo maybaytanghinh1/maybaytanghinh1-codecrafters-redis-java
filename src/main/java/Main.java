91913fcc0fee58444cfb6505beca1775a36df59a
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2024-08-05 16:57:54 +0200

    Try if master host is not null

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -174,20 +174,21 @@ public class Main {
             parsedCommand.get(2)
             );
 
-        
-            for (SocketChannel replica : replicas) {
-                ByteBuffer new_buffer = ByteBuffer.allocate(1024);
-                new_buffer.clear();
-                new_buffer.put(command.getBytes(StandardCharsets.UTF_8));  // Put the command into the buffer
-                new_buffer.flip();  // Prepare buffer for writing
-                try {
-                    replica.write(new_buffer);
-                } catch (IOException e) {
-                    // Handle the exception here
-                    System.err.println("Error writing to replica: " + e.getMessage());
-                    e.printStackTrace();
+            if (master_host != null) {
+                for (SocketChannel replica : replicas) {
+                    ByteBuffer new_buffer = ByteBuffer.allocate(1024);
+                    new_buffer.clear();
+                    new_buffer.put(command.getBytes(StandardCharsets.UTF_8));  // Put the command into the buffer
+                    new_buffer.flip();  // Prepare buffer for writing
+                    try {
+                        replica.write(new_buffer);
+                    } catch (IOException e) {
+                        // Handle the exception here
+                        System.err.println("Error writing to replica: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                     // Write buffer to the replica
                 }
-                 // Write buffer to the replica
             }
         } else if (cmd.equalsIgnoreCase("GET")) {
             ExpiryAndValue cached = cache.get(parsedCommand.get(1));
